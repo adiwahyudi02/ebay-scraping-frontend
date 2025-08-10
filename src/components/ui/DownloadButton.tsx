ff9a72ceee9c22c0286af082c4bc1e5b61e7a32a
Adi Wahyudi <adi.whyd02@gmail.com>
2025-08-10 17:49:21 +0700

    feat: create DownloadButton component

---
+++ src/components/ui/DownloadButton.tsx
@@ -0,0 +1,42 @@
+import { Button } from "./Button";
+
+interface DownloadButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+}
+
+export const DownloadButton: React.FC<DownloadButtonProps> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <Button
+      size="sm"
+      variant="outline"
+      className="inline-flex items-center gap-3"
+      {...props}
+    >
+      <svg
+        className="w-6 h-6 inherit"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z"
+          clip-rule="evenodd"
+        />
+        <path
+          fill-rule="evenodd"
+          d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      {children || "Download"}
+    </Button>
+  );
+};
