5eb82987dde94ba332207d0ac5f3d6f8a64498a6
Adi Wahyudi <adi.whyd02@gmail.com>
2025-08-10 18:01:48 +0700

    feat: add product interfaces

---
+++ src/types/product.d.ts
@@ -0,0 +1,13 @@
+export interface IProduct {
+  title: string;
+  price: string;
+  image: string;
+  link: string;
+  description: string;
+}
+
+export interface IMeta {
+  page: number;
+  size: number;
+  total: number;
+}
