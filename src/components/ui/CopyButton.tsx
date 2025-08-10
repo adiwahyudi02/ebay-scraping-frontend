33b9fbbba80b2d70cfd37b703801c597df180ca7
Adi Wahyudi <adi.whyd02@gmail.com>
2025-08-10 17:49:01 +0700

    feat: create CopyButton component

---
+++ src/components/ui/CopyButton.tsx
@@ -0,0 +1,38 @@
+import { Button } from "./Button";
+
+interface CopyButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+}
+
+export const CopyButton: React.FC<CopyButtonProps> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <Button
+      size="sm"
+      variant="outline"
+      className="inline-flex items-center gap-3"
+      {...props}
+    >
+      <svg
+        className="w-6 h-6 inherit"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 8v3a1 1 0 0 1-1 1H5m11 4h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-7a1 1 0 0 0-1 1v1m4 3v10a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-7.13a1 1 0 0 1 .24-.65L7.7 8.35A1 1 0 0 1 8.46 8H13a1 1 0 0 1 1 1Z"
+        />
+      </svg>
+      {children || "Copy"}
+    </Button>
+  );
+};
